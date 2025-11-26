982d4681d9952c9559b6f024a729ada14bba3999
Zenkovich Andrei <andrei.zenkovich@playrix.com>
2025-11-27 00:33:53 +0400

    Removed spine, fixed windows compilation

--- tests/cpp-tests/Classes/MaterialSystemTest/MaterialSystemTest.cpp
+++ tests/cpp-tests/Classes/MaterialSystemTest/MaterialSystemTest.cpp
@@ -27,7 +27,6 @@
 #include "MaterialSystemTest.h"
 
 #include <ctime>
-#include <spine/spine-cocos2dx.h>
 
 #include "../testResource.h"
 #include "cocos2d.h"
--- tests/cpp-tests/Classes/tests.h
+++ tests/cpp-tests/Classes/tests.h
@@ -98,7 +98,6 @@
 #include "SchedulerTest/SchedulerTest.h"
 #include "ShaderTest/ShaderTest.h"
 #include "ShaderTest/ShaderTest2.h"
-#include "SpineTest/SpineTest.h"
 #include "Sprite3DTest/Sprite3DTest.h"
 #include "SpritePolygonTest/SpritePolygonTest.h"
 #include "SpriteTest/SpriteTest.h"
